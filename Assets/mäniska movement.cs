7d40a5d259ed863d02ab3f5fa997322b991432c2
Hugo Landin <sspel25hula@axevalla.fhsk.se>
2025-11-04 19:07:30 +0100

    human move now

--- Assets/mäniska movement.cs
+++ Assets/mäniska movement.cs
@@ -4,19 +4,30 @@ public class mäniska : MonoBehaviour
 
 {
     public float speed = 1f;
+    private Vector2 direction;
+    private float changeDirectionTime = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //som namnen random väg att gå
+        direction = Random.insideUnitCircle.normalized;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float randomX = Random.Range(-1f, 1f);
-
-        float randomY = Random.Range(-1f, 1f);
-
-        Vector2 randomDirection = new Vector2(randomX, randomY);
+        //väjer random ställe att flytta sig varje ny start på spel
+        changeDirectionTime -= Time.deltaTime;
+        if (changeDirectionTime <= 0f)
+        {
+            direction = Random.insideUnitCircle.normalized;
+            changeDirectionTime = Random.Range(1f, 3f); 
+        }
+        //denna flyttar spelobjektet 
+        transform.Translate(direction * speed * Time.deltaTime);
+        //använd detta för hålla snubbe inom scenen 
+        Vector3 pos = transform.position;
+        if (pos.x > 10 || pos.x < -10) direction.x = -direction.x;
+        if (pos.y > 5 || pos.y < -5) direction.y = -direction.y;
     }
 }
