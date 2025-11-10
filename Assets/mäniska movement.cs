e8302210172e91c3ef7779658a09f853bacec572
Hugo Landin <sspel25hula@axevalla.fhsk.se>
2025-11-10 17:38:28 +0100

    infection funkar

--- Assets/mäniska movement.cs
+++ Assets/mäniska movement.cs
@@ -5,9 +5,13 @@ public class Maniska : MonoBehaviour
     public float speed = 1f;
     private Vector2 direction;
     private float changeDirectionTime = 0f;
+    public enum HealthState { Healthy, Infected, Immune }
+    public HealthState state = HealthState.Healthy;
+    private SpriteRenderer sr;
 
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
         // börjar på random ställe
         direction = Random.insideUnitCircle.normalized;
     }
@@ -58,12 +62,48 @@ public class Maniska : MonoBehaviour
 
         // aplayar in eller något in den rätta positionen 
         transform.position = pos;
+        switch (state)
+        {
+            case HealthState.Healthy: sr.color = Color.green; break;
+            case HealthState.Infected: sr.color = Color.red; break;
+            case HealthState.Immune: sr.color = Color.blue; break;
+        }
+
     }
 
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("i hit" + other.name);
-        
+        Maniska otherHuman = other.GetComponent<Maniska>();
+        if (otherHuman != null)
+        {
+            Debug.Log($"{name} touched {other.name}");
+
+            
+            if ((state == HealthState.Infected && otherHuman.state != HealthState.Immune) ||
+                (otherHuman.state == HealthState.Infected && state != HealthState.Immune))
+            {
+                int roll = Random.Range(0, 100);
+
+                if (roll < 40)
+                {
+                    state = HealthState.Infected;
+                    otherHuman.state = HealthState.Infected;
+                    Debug.Log($"Infection! Roll = {roll}");
+                }
+                else if (roll <99)
+                {
+                    
+                    Debug.Log($"Safe this time. Roll = {roll}");
+                }
+                else
+                {
+                    
+                    state = HealthState.Immune;
+                    otherHuman.state = HealthState.Immune;
+                    Debug.Log($"Immune! Roll = {roll}");
+                }
+            }
+        }
     }
 }
